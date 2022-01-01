c94f8026b1472eb0f5564ed31f7ef8c51cb85009
Aizuddin Badry <aizuddinbadry@outlook.com>
2022-01-01 04:52:54 -0800

    Removed unused deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,17 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import * as React from "react";
-import { ApolloProvider } from "react-apollo";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+
+import "./index.css";
 
 import App from "./App";
 
 const GRAPHQL_API_URL = "http://localhost:4000/graphql";
 
 const client = new ApolloClient({
-  clientState: {
-    resolvers: {
-      Query: {
-        localHello(obj: any, { subject }: { subject: string }) {
-          return `Hello, ${subject}! from Web UI`;
-        },
-      },
-    },
-  },
   uri: GRAPHQL_API_URL,
+  cache: new InMemoryCache(),
 });
 
 render(
